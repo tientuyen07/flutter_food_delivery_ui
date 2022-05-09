be09f784091c4eeb5796944f144de4404b5706e3
tientuyen <tientuyen07@gmail.com>
2022-05-09 22:40:11 +0700

    [Init project] [Init project food delivery]

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_food_delivery_ui/data/data.dart';
+import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
+import 'package:flutter_food_delivery_ui/widgets/recent_orders.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  _buildRestaurants() {
+    List<Widget> restaurantList = [];
+    restaurants.forEach((restaurant) {
+      restaurantList.add(Container(
+        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15.0),
+          border: Border.all(color: Colors.grey[200], width: 1.0),
+        ),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image(
+                height: 150,
+                width: 150,
+                image: AssetImage(restaurant.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(12.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    restaurant.name,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 4.0),
+                  RatingStars(restaurant.rating),
+                  SizedBox(height: 6.0),
+                  Text(
+                    restaurant.address,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 4.0),
+                  Text(
+                    '0.2 miles away',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ));
+    });
+    return Column(children: restaurantList);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.account_circle),
+          iconSize: 30.0,
+          onPressed: () {},
+        ),
+        title: Text('Food Delivery'),
+        actions: <Widget>[
+          TextButton(
+            style: TextButton.styleFrom(
+                minimumSize: Size(30.0, 30.0),
+                primary: Colors.grey,
+                padding: EdgeInsets.all(0),
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                )),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+              child: Text(
+                'Cart (${currentUser.cart.length})',
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: ListView(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(width: 0.8),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                  borderSide: BorderSide(
+                      width: 0.8, color: Theme.of(context).primaryColor),
+                ),
+                hintText: 'Search Food or Restaurants',
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.only(left: 15.0, right: 8.0),
+                  child: Icon(
+                    Icons.search,
+                    size: 30.0,
+                  ),
+                ),
+                suffixIcon: Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ),
+          ),
+          RecentOrders(),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text(
+                  'Nearby Restaurants',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.2,
+                  ),
+                ),
+              ),
+              _buildRestaurants()
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/rating_stars.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class RatingStars extends StatelessWidget {
+  const RatingStars(this.rating, {Key key}) : super(key: key);
+  final int rating;
+
+  @override
+  Widget build(BuildContext context) {
+    String stars = '';
+    for (int i = 0; i < rating; i++) {
+      stars += '⭐  ';
+    }
+    stars.trim();
+    return Text(
+      stars,
+      style: TextStyle(
+        fontSize: 18.0,
+      ),
+    );
+  }
+}
